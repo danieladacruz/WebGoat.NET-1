250ea30856d0708557a3e9a57fdb2db3e10928c3
Daniela da Cruz <daniela.dacruz@checkmarx.com>
2019-01-08 23:44:56 +0000

    Fix SQLInjection (CWE-89) in GetCustomerEmails

--- WebGoat/App_Code/DB/MySqlDbProvider.cs
+++ WebGoat/App_Code/DB/MySqlDbProvider.cs
@@ -556,12 +556,13 @@ namespace OWASP.WebGoat.NET.App_Code.DB
 
         public DataSet GetCustomerEmails(string email)
         {
-            string sql = "select email from CustomerLogin where email like '" + email + "%'";
-            
+            string sql = "select email from CustomerLogin where email like ?email";
             
             using (MySqlConnection connection = new MySqlConnection(_connectionString))
             {
-                MySqlDataAdapter da = new MySqlDataAdapter(sql, connection);
+                MySqlCommand m = new MySqlCommand(sql);
+                m.Parameters.AddWithValue("@email", email);
+                MySqlDataAdapter da = new MySqlDataAdapter(m);
                 DataSet ds = new DataSet();
                 da.Fill(ds);
 
